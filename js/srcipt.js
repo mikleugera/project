31f7c12add398b35bee2f59c005e78fa3f9c4935
Mikle <ugeramikle@gmail.com>
2021-11-30 22:13:22 +0200

    first program

--- js/srcipt.js
+++ js/srcipt.js
@@ -1,2 +1,19 @@
-const name = 'Mikle';
-alert(`Hello, ${name}`);
+const numberOdFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+
+const personMovieDB ={
+    count: numberOdFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?','');
+
+personMovieDB.movies[a] = b;
+personMovieDB.movies[c] = d;
+
+console.log(personMovieDB);
